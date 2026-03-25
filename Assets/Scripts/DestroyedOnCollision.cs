63a466cc3f1f8e8e9b3bf80587252504528af983
ozzybodkin <OMS008@student.usc.edu.au>
2026-03-25 10:57:12 +1000

    minor formatting changes

--- Assets/Scripts/DestroyedOnCollision.cs
+++ Assets/Scripts/DestroyedOnCollision.cs
@@ -23,14 +23,12 @@ public class DestroyedOnCollision : MonoBehaviour
     {
         bool tagInList = tags.Contains(other.gameObject.tag);
 
-        if (tagListType == TagListType.Blacklist 
-            && tagInList)
+        if (tagListType == TagListType.Blacklist && tagInList)
         {
             // Destroy if it's a Blacklist and the tag IS in the Blacklist
             Destroy(gameObject);
         }
-        else if (tagListType == TagListType.Whitelist 
-            && !tagInList)
+        else if (tagListType == TagListType.Whitelist && !tagInList)
         {
             // Destroy if it's a Whitelist and the tag is NOT in the Whitelist
             Destroy(gameObject);
--- Assets/Scripts/ShootingScript.cs
+++ Assets/Scripts/ShootingScript.cs
@@ -3,7 +3,6 @@ using System.Collections;
 
 public class ShootingScript : MonoBehaviour
 {
-
     [SerializeField]
     private GameObject bullet;
 
